033561bb8513c7bf33858db75d0dc24f1b249897
fede1712 <fedemarinasruiz@gmail.com>
2024-06-20 12:09:22 +0200

    add package

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_Dtpijb7H.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_CZiQO6RM.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_Dmk3_khs.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_CNrp65yU.mjs');
+
+export { page };
