5c5897f69fb68ccf745ad1bd7cac534d633febb2
Fede Marinas <fedemarinasruiz@gmail.com>
2024-09-09 10:19:55 +0200

    redeploy

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_Dtpijb7H.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_CZiQO6RM.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_Dmk3_khs.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_CNrp65yU.mjs');
-
-export { page };
